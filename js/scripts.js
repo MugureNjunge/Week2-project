78b10c78bf87fbf66407972604e336631379ff2a
MugureNjunge <maureennjunge22@gmail.com>
2022-03-06 00:48:11 +0300

    add the gender function

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,13 @@ function conversion () {
   let daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
   let maleAkanNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
   let femaleAkanNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+
+  function getGender () {
+    for (let gender of genders){
+      if (gender.checked){
+        return gender.value;
+      }
+    }
+  }
+
+  let myGenderValue = getGender();
