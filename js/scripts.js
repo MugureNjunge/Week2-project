f01c8991198f784fe4a5e689c89bdff0ced89731
MugureNjunge <maureennjunge22@gmail.com>
2022-03-06 00:43:09 +0300

    add variables to my js file

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+function conversion () {
+  let yearOfBirth = document.getElementById("yyyy").value;
+  let monthOfBirth = parseInt(document.getElementById("mm").value);
+  let dayOfBirth = parseInt(document.getElementById("dd").value);
+  let genders = document.getElementsByName("gender");
+  let daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  let maleAkanNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+  let femaleAkanNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
